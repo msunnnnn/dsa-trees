b0560a7d7d3e48bd1472a105068f4c4f2936d813
Margaret Sun <hi.margaret.sun@gmail.com>
2022-09-21 11:48:47 -0700

    finish tree.js

--- tree.js
+++ tree.js
@@ -13,14 +13,61 @@ class Tree {
   }
 
   /** sumValues(): add up all of the values in the tree. */
+  //check if arr is empty
+  // need current node to be last item in array
+  // get the node's value and add to total
+  // loop through node's children
+  // if that node has children, add to array
 
-  sumValues() {
-    
+  sumValues(val) {
+    if (!this.root) {
+      return 0;
+    }
+
+    let total = 0;
+    let arr = [this.root];
+
+    while (arr.length) {
+      let current = arr.pop();
+
+      total += current.val;
+
+      for (let child of current.children) {
+        arr.push(child);
+      }
+    }
+    return total;
   }
+
+
+
+
+
+
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if (!this.root) {
+      return 0;
+    }
+
+    let total = 0;
+    let arr = [this.root];
+
+    while (arr.length) {
+      let current = arr.pop();
+
+      if (current.val % 2 === 0) {
+        total++;
+      }
+      for (let child of current.children) {
+        arr.push(child);
+      }
+    }
+    return total;
+
+
 
   }
 
@@ -28,6 +75,24 @@ class Tree {
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (!this.root) {
+      return 0;
+    }
+
+    let total = 0;
+    let arr = [this.root];
+
+    while (arr.length) {
+      let current = arr.pop();
+
+      if (current.val > lowerBound) {
+        total++;
+      }
+      for (let child of current.children) {
+        arr.push(child);
+      }
+    }
+    return total;
 
   }
 }
