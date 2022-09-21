20d7a95aa6018dc478def786415b21c29d9993c4
Margaret Sun <hi.margaret.sun@gmail.com>
2022-09-21 13:28:42 -0700

    recursion for min/max depth

--- binary-tree.js
+++ binary-tree.js
@@ -17,6 +17,32 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth(node = this.root) {
+    //start at 1 (a)
+    // queue (breadth search) check if second layer has children
+    //  add counter for each layer
+    // if it doesn't have children return counter
+    if(!node){
+      return 0
+    }
+    let counter = 1
+    // let queue = [node]
+
+    // while(queue.length){
+    //   counter++
+    //   let current = queue.shift()
+    //   if (!current.children){
+    //     return counter
+    //   }
+
+    //   for (let child of current.children){
+    //     queue.push(child)
+    //   }
+
+    // }
+
+    return Math.min(this.minDepth(node.left), this.minDepth(node.right)) + counter
+
+
 
   }
 
@@ -24,13 +50,23 @@ class BinaryTree {
    * the length of the longest path from the root to a leaf. */
 
   maxDepth(node = this.root) {
+    //if a has children -> look into c
+    if(!node){
+      return 0
+    }
+    let counter = 1
+    return Math.max(this.maxDepth(node.left), this.maxDepth(node.right)) + counter
 
   }
+
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
   nextLarger(lowerBound, node = this.root) {
+    if(!node){
+      return null
+    }
 
   }
 
